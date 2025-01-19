19f227e9036916fbc07138342799c1630c021392
markpeach59 <mark.peach@frontbacktech.com>
2025-01-19 14:42:33 +0000

    chargers defined

--- src/components/forkliftdetail.jsx
+++ src/components/forkliftdetail.jsx
@@ -1010,6 +1010,7 @@ class ForkliftDetail extends Component {
 
       
       console.log ('Chargers', battery.chargers)
+     /* what about chargers on their own */
 
       if (battery.chargers !== undefined){
         this.setState({
