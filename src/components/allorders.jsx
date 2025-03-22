5544c4c2e8707cf2bfeef4331a05b52202e38424
markpeach59 <mark.peach@frontbacktech.com>
2025-03-22 14:26:28 +0000

    functional - no classes

--- src/components/allorders.jsx
+++ src/components/allorders.jsx
@@ -1,206 +1,183 @@
 import _ from "lodash";
-import React, { Component } from "react";
-
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import { getAllOrders } from "../services/allOrdersService";
 import { confirmOrder } from "../services/ordersService";
 import { getUsers } from "../services/userService";
 import { getDealers } from "../services/dealerService";
-
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-
 import Table from "@mui/material/Table";
-//import TableHead from "@mui/material/TableHead";
-
 import TableBody from "@mui/material/TableBody";
-
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-
 import TableHead from "@mui/material/TableHead";
 import Confirmorder from "./confirmorder";
-
 import "typeface-roboto";
 
-class Orders extends Component {
-  state = {
-    orders: [],
-    user: [],
-    dealers: [],
-  };
+const Orders = () => {
+  const [orders, setOrders] = useState([]);
+  const [users, setUsers] = useState([]);
+  const [dealers, setDealers] = useState([]);
 
-  async componentDidMount() {
-    const { data: orders } = await getAllOrders();
-    //console.log("Orders Returned", orders);
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const { data: ordersData } = await getAllOrders();
+        const { data: usersData } = await getUsers();
+        const { data: dealersData } = await getDealers();
 
-    const { data: users } = await getUsers();
-    //console.log("Users Returned", users);
+        setOrders(ordersData);
+        setUsers(usersData);
+        setDealers(dealersData);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-    const { data: dealers } = await getDealers();
-    //console.log("Dealers Returned", dealers);
+    fetchData();
+  }, []);
 
-    this.setState({
-      orders,
-      users,
-      dealers,
-    });
-  }
-
-  handleConfirmation = async (orderid, stocknumber) => {
-    
-    //console.log("Confirmation ", orderid);
-
-    const theorder = _.find(this.state.orders, ["_id", orderid]);
+  const handleConfirmation = async (orderid, stocknumber) => {
+    const theorder = _.find(orders, ["_id", orderid]);
 
     if (theorder === undefined) return;
 
     console.log("the Order", theorder);
-    theorder.confirmedorder = true;
-    theorder.stocknumber = stocknumber;
-
-    this.setState({ theorder });
-
-    // need to store this back in MongoDB
-
-   
-    try {
-      await confirmOrder(orderid, stocknumber)
-    } catch(error){
-      console.log("Could not Confirm Order ", orderid, " in DB");
-      // should be resetting markup to prev value
-    }
     
+    // Create a new array with the updated order
+    const updatedOrders = orders.map(order => {
+      if (order._id === orderid) {
+        return {
+          ...order,
+          confirmedorder: true,
+          stocknumber: stocknumber
+        };
+      }
+      return order;
+    });
+    
+    setOrders(updatedOrders);
+
+    // Store this back in MongoDB
+    try {
+      await confirmOrder(orderid, stocknumber);
+    } catch (error) {
+      console.log("Could not Confirm Order ", orderid, " in DB");
+      // Should be resetting to previous state
+    }
   };
 
-  render() {
-    const t = this.state.orders;
-    const u = this.state.users;
-    const d = this.state.dealers;
-    //console.log("LL", t);
+  const dealername = (id) => {
+    const ptr = _.find(users, ["_id", id]);
 
-    const { length: count } = this.state.orders;
+    if (ptr === undefined) return " - ";
 
-    if (count === 0) return <p>There are no Orders in the database</p>;
+    const m = ptr.dealerId;
 
-    const dealername = (id) => {
-      //console.log(id);
+    if (m === undefined) return " - ";
 
-      const ptr = _.find(u, ["_id", id]);
+    const dealer = _.find(dealers, ["_id", m]);
+    if (!dealer) return " - ";
+    
+    return dealer.dealername;
+  };
 
-      if (ptr === undefined) return " - ";
+  const username = (id) => {
+    const ptr = _.find(users, ["_id", id]);
 
-      const m = ptr.dealerId;
+    if (ptr === undefined) return " - ";
 
-      if (m === undefined) return " - ";
+    return ptr.name;
+  };
 
-      const dn = _.find(d, ["_id", m]).dealername;
-      return dn;
-    };
+  const emailaddr = (id) => {
+    const ptr = _.find(users, ["_id", id]);
 
-    const username = (id) => {
-      //console.log(id);
+    if (ptr === undefined) return " - ";
 
-      const ptr = _.find(u, ["_id", id]);
+    return ptr.email;
+  };
 
-      if (ptr === undefined) return " - ";
+  const { length: count } = orders;
 
-      return ptr.name;
-    };
+  if (count === 0) return <p>There are no Orders in the database</p>;
 
-    const emailaddr = (id) => {
-      //console.log(id);
-      
-      const ptr = _.find(u, ["_id", id]);
+  return (
+    <React.Fragment>
+      <Grid container spacing={2}>
+        <Grid item xs={10}>
+          <h2>List of Orders</h2>
 
-      if (ptr === undefined) return " - ";
-
-      return ptr.email;
-    };
-
-    return (
-      <React.Fragment>
-        <Grid container spacing={2}>
-          <Grid item xs={10}>
-            <h2>List of Orders</h2>
-
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Date & Time</TableCell>
-                  <TableCell align="right">Name</TableCell>
-                  <TableCell>Email</TableCell>
-                  <TableCell>Dealer</TableCell>
-                  <TableCell align="right">Model</TableCell>
-                  <TableCell align="right">Offer</TableCell>
-                  <TableCell align="right">Saving</TableCell>
-                  <TableCell align="right">Total</TableCell>
-                  <TableCell align="right">Confirmed</TableCell>
-                  <TableCell align="right">PO#</TableCell>
-                  <TableCell align="right">Stock#</TableCell>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Date & Time</TableCell>
+                <TableCell align="right">Name</TableCell>
+                <TableCell>Email</TableCell>
+                <TableCell>Dealer</TableCell>
+                <TableCell align="right">Model</TableCell>
+                <TableCell align="right">Offer</TableCell>
+                <TableCell align="right">Saving</TableCell>
+                <TableCell align="right">Total</TableCell>
+                <TableCell align="right">Confirmed</TableCell>
+                <TableCell align="right">PO#</TableCell>
+                <TableCell align="right">Stock#</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {orders.map((x) => (
+                <TableRow key={x._id}>
+                  <TableCell>
+                    <Link to={{ pathname: "/orders/" + x._id }}>
+                      <Button>
+                        {_.slice(x.updatedAt, 0, 10)}{" "}
+                        {_.slice(x.updatedAt, 11, 19)}
+                      </Button>
+                    </Link>
+                  </TableCell>
+                  <TableCell align="right">
+                    {" "}
+                    {username(x.userid)}
+                  </TableCell>
+                  <TableCell align="right">
+                    {" "}
+                    {emailaddr(x.userid)}
+                  </TableCell>
+                  <TableCell align="right"> {dealername(x.userid)}</TableCell>
+                  <TableCell align="right"> {x.model}</TableCell>
+                  <TableCell align="right">
+                    {x.offer ? "Yes" : null}
+                  </TableCell>
+                  <TableCell align="right">
+                    {x.offer ? " £" + x.saving : null}
+                  </TableCell>
+                  <TableCell align="right">
+                    {x.offer ? " £" + (x.price - x.saving) : " £" + x.price}
+                  </TableCell>
+                  <TableCell align="right">
+                    {x.confirmedorder && "YES"}
+                    {!x.confirmedorder && (
+                      <Confirmorder
+                        orderid={x._id}
+                        onConfirmorder={handleConfirmation}
+                      />
+                    )}
+                  </TableCell>
+                  <TableCell align="right">
+                    {!x.ponumber ? "" : x.ponumber}
+                  </TableCell>
+                  <TableCell align="right">
+                    {!x.stocknumber ? "" : x.stocknumber}
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {t.map((x) => (
-                  <TableRow key={x._id}>
-                    <TableCell>
-                      <Link to={{ pathname: "/orders/" + x._id }}>
-                        <Button>
-                          {_.slice(x.updatedAt, 0, 10)}{" "}
-                          {_.slice(x.updatedAt, 11, 19)}
-                        </Button>
-                      </Link>
-                    </TableCell>
-                    <TableCell align="right">
-                      {" "}
-                      {username(x.userid)}
-                
-                    </TableCell>
-                    <TableCell align="right">
-                      {" "}
-                      {emailaddr(x.userid)}
-                    </TableCell>
-                    <TableCell align="right"> {dealername(x.userid)}</TableCell>
-                    <TableCell align="right"> {x.model}</TableCell>
-                    <TableCell align="right">
-                     
-                     {x.offer  ? ( "Yes") : null}
-                     </TableCell>
- 
-                     <TableCell align="right">
-                     {x.offer  ? ( " £" + x.saving) : null}
-                     </TableCell>
- 
- 
-                     <TableCell align="right">
-                     {x.offer  ? (" £" + (x.price - x.saving)): (" £" + (x.price ))}
-                     </TableCell>
-                     <TableCell align="right">
-                     {x.confirmedorder && "YES"}
-                      {!x.confirmedorder && 
-                     <Confirmorder  
-                     orderid={x._id}
-                     onConfirmorder={this.handleConfirmation} />
-                    }
-                     </TableCell>
-
-                     <TableCell align="right">
-                     {!x.ponumber? "": x.ponumber}
-                    </TableCell>
-                    <TableCell align="right">
-                    {!x.stocknumber? "": x.stocknumber}
-                    </TableCell>
-
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Grid>
+              ))}
+            </TableBody>
+          </Table>
         </Grid>
-      </React.Fragment>
-    );
-  }
-}
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 export default Orders;
--- src/components/quotes.jsx
+++ src/components/quotes.jsx
@@ -1,121 +1,106 @@
 import _ from "lodash";
-import React, { Component } from "react";
-
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-
 import { getQuotes } from "../services/quotesService";
-
 import Grid from "@mui/material/Grid";
 import Button from "@mui/material/Button";
-
 import Table from "@mui/material/Table";
-//import TableHead from "@mui/material/TableHead";
-
 import TableBody from "@mui/material/TableBody";
-
 import TableRow from "@mui/material/TableRow";
 import TableCell from "@mui/material/TableCell";
-
 import TableHead from "@mui/material/TableHead";
-
 import "typeface-roboto";
 
-class Quotes extends Component {
-  state = {
-    quotes: [],
-  };
+const Quotes = () => {
+  const [quotes, setQuotes] = useState([]);
 
-  async componentDidMount() {
-    try {
-      console.log("Fetching quotes...");
-      const response = await getQuotes();
-      console.log("Quotes API Response:", response);
-      
-      if (response && response.data) {
-        console.log("Quotes Returned", response.data);
-        this.setState({
-          quotes: response.data,
-        });
-      } else {
-        console.error("No data property in response:", response);
-        this.setState({ quotes: [] });
+  useEffect(() => {
+    const fetchQuotes = async () => {
+      try {
+        console.log("Fetching quotes...");
+        const response = await getQuotes();
+        console.log("Quotes API Response:", response);
+        
+        if (response && response.data) {
+          console.log("Quotes Returned", response.data);
+          setQuotes(response.data);
+        } else {
+          console.error("No data property in response:", response);
+          setQuotes([]);
+        }
+      } catch (error) {
+        console.error("Error fetching quotes:", error);
+        setQuotes([]);
       }
-    } catch (error) {
-      console.error("Error fetching quotes:", error);
-      this.setState({ quotes: [] });
-    }
-  }
+    };
 
-  render() {
-    const t = this.state.quotes;
-    //console.log("LL", t);
+    fetchQuotes();
+  }, []);
 
-    const { length: count } = this.state.quotes;
+  const { length: count } = quotes;
 
-    if (count === 0) return <p>There are no quotes in the database</p>;
+  if (count === 0) return <p>There are no quotes in the database</p>;
 
-    return (
-      <React.Fragment>
-        <Grid container spacing={2}>
-          <Grid item xs={8}>
-            <h2>List of Quotes</h2>
+  return (
+    <React.Fragment>
+      <Grid container spacing={2}>
+        <Grid item xs={8}>
+          <h2>List of Quotes</h2>
 
-            <Table>
-              <TableHead>
-                <TableRow>
-                  <TableCell>Date & Time</TableCell>
-                  <TableCell align="right">Model</TableCell>
-                  <TableCell align="right">Cost</TableCell>
-                  <TableCell align="right">Markup</TableCell>
-                  <TableCell align="right">Offer</TableCell>
-                  <TableCell align="right">Saving</TableCell>
-                  <TableCell align="right">Total</TableCell>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Date & Time</TableCell>
+                <TableCell align="right">Model</TableCell>
+                <TableCell align="right">Cost</TableCell>
+                <TableCell align="right">Markup</TableCell>
+                <TableCell align="right">Offer</TableCell>
+                <TableCell align="right">Saving</TableCell>
+                <TableCell align="right">Total</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              {quotes.map((x) => (
+                <TableRow key={x._id}>
+                  <TableCell>
+                    <Link to={{ pathname: "/quotes/" + x._id }}>
+                      <Button>
+                        {_.slice(x.createdAt, 0, 10)}{" "}
+                        {_.slice(x.createdAt, 11, 19)}
+                      </Button>
+                    </Link>
+                  </TableCell>
+                  <TableCell align="right"> {x.model}</TableCell>
+                  <TableCell align="right">
+                    {" £"}
+                    {x.price}
+                  </TableCell>
+                  <TableCell align="right">
+                    {" £"}
+                    {x.markup}
+                  </TableCell>
+
+                  <TableCell align="right">
+                    {x.offer ? "Yes" : null}
+                  </TableCell>
+
+                  <TableCell align="right">
+                    {x.offer ? " £" + x.saving : null}
+                  </TableCell>
+
+                  <TableCell align="right">
+                    {x.offer 
+                      ? " £" + (x.price + x.markup - x.saving)
+                      : " £" + (x.price + x.markup)}
+                  </TableCell>
                 </TableRow>
-              </TableHead>
-              <TableBody>
-                {t.map((x) => (
-                  <TableRow key={x._id}>
-                    <TableCell>
-                      <Link to={{ pathname: "/quotes/" + x._id }}>
-                        <Button>
-                          {_.slice(x.createdAt, 0, 10)}{" "}
-                          {_.slice(x.createdAt, 11, 19)}
-                        </Button>
-                      </Link>
-                    </TableCell>
-                    <TableCell align="right"> {x.model}</TableCell>
-                    <TableCell align="right">
-                      {" £"}
-                      {x.price}
-                    </TableCell>
-                    <TableCell align="right">
-                      {" £"}
-                      {x.markup}
-                    </TableCell>
-
-                    <TableCell align="right">
-                     
-                    {x.offer  ? ( "Yes") : null}
-                    </TableCell>
-
-                    <TableCell align="right">
-                    {x.offer  ? ( " £" + x.saving) : null}
-                    </TableCell>
-
-
-                    <TableCell align="right">
-                    {x.offer  ? (" £" + (x.price + x.markup - x.saving)): (" £" + (x.price + x.markup))}
-                    </TableCell>
-
-                  </TableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </Grid>
+              ))}
+            </TableBody>
+          </Table>
         </Grid>
-      </React.Fragment>
-    );
-  }
-}
+      </Grid>
+    </React.Fragment>
+  );
+};
 
 export default Quotes;
